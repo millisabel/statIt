e41f77f34b8f3b3300d5b68b176efceccdad4d05
millisabel <38396309+millisabel@users.noreply.github.com>
2021-04-28 14:07:08 +0300

    init: deploy

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,44 +1,25 @@
 import React, {useState} from 'react';
 import comment from "./comment.module.css";
+import TextareaAutosize from 'react-autosize-textarea';
 
 const Comment = (props) => {
     const initComment = 'Feedback';
     const[userComment, setComment] = useState('');
-    const[rowsLength, setRowsLength] = useState(1);
-    const cols = 38;
-    const max = 200;
 
     const handleOut = (e) => {
         setComment(e.target.value);
         return userComment;
     };
 
-    const handleKeyDown = (e) => {
-        if(e.keyCode === 46){
-            setRowsLength(1);
-            setComment('');
-        }
-        if (e.target.value.length % cols === 0 && e.target.value.length < max && e.target.value.length >= cols) {
-            if(e.keyCode === 8){
-                setRowsLength(rowsLength - 1);
-            } else{
-                setRowsLength(rowsLength + 1);
-            }
-        }
-        return e.target.innerText;
-    };
-
     return (
-        <textarea
+        <TextareaAutosize
             title='max-length 200 char'
             placeholder={initComment}
-            maxLength={max}
-            cols={cols}
-            rows={rowsLength}
+            maxLength={200}
+            rows={1}
             value={userComment}
             className={comment.item}
             onChange={(e) => props.onClick(handleOut(e))}
-            onKeyDown = {(e) => props.onClick(handleKeyDown(e))}
         />
     );
 };
