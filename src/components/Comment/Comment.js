913bbd9f1846f631dff6ba9732c690496bb876ae
millisabel <38396309+millisabel@users.noreply.github.com>
2021-06-10 17:16:24 +0300

    update: updated Comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -5,22 +5,35 @@ import TextareaAutosize from 'react-autosize-textarea';
 const Comment = (props) => {
     const initComment = 'Feedback';
     const[userComment, setComment] = useState('');
+    const[mess, setMess] = useState('');
 
     const handleOut = (e) => {
         setComment(e.target.value);
+        if(e.target.value.length === 200){
+            setMess('maximum number of characters');
+        }
+        else if(!e.target.value.length){
+            setMess('');
+        }
+        else{
+            setMess('min 3 - max 200 characters');
+        }
         return userComment;
     };
 
     return (
-        <TextareaAutosize
-            title='max-length 200 char'
-            placeholder={initComment}
-            maxLength={200}
-            rows={1}
-            value={userComment}
-            className={comment.item}
-            onChange={(e) => props.onClick(handleOut(e))}
-        />
+        <div className={comment.box}>
+            <TextareaAutosize
+                title='max-length 200 char'
+                placeholder={initComment}
+                maxLength={200}
+                rows={1}
+                value={userComment}
+                className={comment.text}
+                onChange={(e) => props.onClick(handleOut(e))}
+            />
+            <p className={comment.mess}>{mess}</p>
+        </div>
     );
 };
 
