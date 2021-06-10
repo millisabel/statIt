aa7fbd2c780730ba10b187ed2ac631ce3994a572
millisabel <38396309+millisabel@users.noreply.github.com>
2021-06-10 09:01:53 +0300

    update: updated project

--- src/components/AddFile/AddFile.js
+++ src/components/AddFile/AddFile.js
@@ -1,26 +1,15 @@
 import React from 'react';
-// import addFile from "../ico/add-file.svg";
-import addPhoto from "../ico/add-photo.svg";
-
-// import ImageUploader from 'react-images-upload';
-import Camera from 'react-html5-camera-photo';
-// import 'react-html5-camera-photo/build/css/index.css';
 
 import file from "./AddFile.module.css";
 import ImageUploading from 'react-images-uploading';
 
 const AddFile = () => {
-    const [camera, setCamera] = React.useState(false);
     const [images, setImages] = React.useState([]);
     const maxNumber = 3;
 
     const onChange = (imageList, addUpdateIndex) => {
         console.log(imageList, addUpdateIndex);
         setImages(imageList);
-    };
-
-    const onClick = () => {
-        setCamera(true);
     };
 
     return (
@@ -42,11 +31,7 @@ const AddFile = () => {
                             <button className={file.btnAdd}
                                     onClick={onImageUpload}
                             />
-                            <button className={file.btnAddPhoto}
-                                    onClick={onClick}
-                            />
                         </div>
-                        {camera ? <Camera/> : null}
                         <div className={file.preview}>
                             {imageList.map((image, index) => (
                                 <div key={index} className={file.preview_item}>
