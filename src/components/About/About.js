87af957d7256b23e64853e7e8d24652789e60434
millisabel <38396309+millisabel@users.noreply.github.com>
2021-06-23 21:19:25 +0300

    feat: added test for component About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,28 +2,24 @@ import React from 'react';
 import about from "./about.module.css";
 import Image from "../Image/Image";
 
-const About = (props) => {
+const About = ({businessName, businessAddress, businessPoint, logo}) => {
     return (
         <div className={about.container}>
             <div className={about.item}>
                 <p>
-                    {/*<span className={about.title}>Name: </span>*/}
-                    <span className={about.title}>{props.businessName}</span>
+                    <span className={about.title}>{businessName}</span>
                 </p>
                 <p>
-                    {/*<span className={about.title}>Address: </span>*/}
-                    <span className={about.title}>{props.businessAddress}</span>
+                    <span className={about.title}>{businessAddress}</span>
                 </p>
                 <p>
-                    {/*<span className={about.title}>Ticket window: </span>*/}
-                    <span className={about.title}>{props.businessPoint}</span>
+                    <span className={about.title}>{businessPoint}</span>
                 </p>
             </div>
             <div className={about.item}>
                 <Image
-                    src = {`${props.logo}`}
-                    // name = {`${props.businessName}`}
-                    alt = {`${props.logo}`}
+                    src = {`${logo}`}
+                    alt = "logo"
                 />
             </div>
         </div>
